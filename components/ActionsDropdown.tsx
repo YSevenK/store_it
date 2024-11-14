2d1d5d09f0da89710aeb9bd3fdb92a632b87addf
ysevenk <ysevenk@qq.com>
2024-11-14 13:31:45 +0800

    File Details

--- components/ActionsDropdown.tsx
+++ components/ActionsDropdown.tsx
@@ -30,6 +30,7 @@ import { Input } from './ui/input';
 import { Button } from './ui/button';
 import { renameFile } from '@/lib/actions/file.actions';
 import { usePathname } from 'next/navigation';
+import { FileDetails } from './ActionsModalContent';
 
 
 
@@ -79,8 +80,9 @@ const ActionsDropdown = ({ file }: { file: Models.Document }) => {
                 <DialogHeader className='flex flex-col gap-3'>
                     <DialogTitle className='text-center text-light-100'>{label}</DialogTitle>
                     {value === 'rename' && (
-                        <Input type='text' value={name} onChange={(e) => setName(e.target.value)} placeholder='Enter new name' />
+                        <Input type='text' value={name} onChange={(e) => setName(e.target.value)} />
                     )}
+                    {value === 'details' && <FileDetails file={file} />}
                 </DialogHeader>
                 {['rename', 'delete', 'share'].includes(value) && (
                     <DialogFooter className='flex flex-col gap-3 md:flex-row'>
